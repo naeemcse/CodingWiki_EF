7a8ea54403fcda0751bfc44b4a86c50970a74905
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2025-12-28 12:28:06 +0600

    1st migration

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-8DI1SVB\\SQLEXPRESS;Database=CodingWikiDB;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+
+    }
+
+}
